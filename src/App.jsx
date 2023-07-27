55f196ef185b85aa43ae4a767703b684bf705434
Cristian Pineda <cristiap17@gmail.com>
2023-07-27 18:24:59 -0500

    Fix styles and bugs in the game_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import ConfigurationScreen from './components/configScreen/ConfigurationScreen'
 import GameScreen from './components/gameScreen/GameScreen'
-import LockOrientationScreen from './components/lockOrientationScreen/lockOrientationScreen'
 import { MemoryAppProvider } from './context/MemoryAppProvider'
 
 function App() {
